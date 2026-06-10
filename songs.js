a115217ac35b69c578bc77bc3c68e0bfe0eff7c4
demahareebum-a11y <demaha.reebum@gmail.com>
2026-06-10 19:39:06 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -1,6 +1,86 @@
 // Automatically generated songs database
 const songsData = {
   "cloud": [
+    {
+      "id": "cloud-01",
+      "title": "Surah Al-Fatiha",
+      "surahNo": 01,
+      "url": "https://res.cloudinary.com/dnudhbjle/video/upload/v1781099661/001_-_Surah_Al-Fatiha_qrmukj.mp3",
+      "artist": "Idris Abkar",
+      "album": "Cloud Recitations",
+      "cover": "https://res.cloudinary.com/dnudhbjle/image/upload/q_auto/f_auto/v1781086631/idrees-abkar-banner_kmq9ly.jpg",
+      "isCloud": true
+    },
+    {
+      "id": "cloud-03",
+      "title": "Surah Aal-E-Imran",
+      "surahNo": 03,
+      "url": "https://res.cloudinary.com/dnudhbjle/video/upload/v1781099792/003_-_Surah_Aal-E-Imran_nismyp.mp3",
+      "artist": "Idris Abkar",
+      "album": "Cloud Recitations",
+      "cover": "https://res.cloudinary.com/dnudhbjle/image/upload/q_auto/f_auto/v1781086631/idrees-abkar-banner_kmq9ly.jpg",
+      "isCloud": true
+    },
+    {
+      "id": "cloud-04",
+      "title": "Surah An-Nisa",
+      "surahNo": 04,
+      "url": "https://res.cloudinary.com/dnudhbjle/video/upload/v1781099791/004_-_Surah_An-Nisa_qetaeb.mp3",
+      "artist": "Idris Abkar",
+      "album": "Cloud Recitations",
+      "cover": "https://res.cloudinary.com/dnudhbjle/image/upload/q_auto/f_auto/v1781086631/idrees-abkar-banner_kmq9ly.jpg",
+      "isCloud": true
+    },    
+    {
+      "id": "cloud-05",
+      "title": "Surah Al-Maeda",
+      "surahNo": 05,
+      "url": "https://res.cloudinary.com/dnudhbjle/video/upload/v1781099785/005_-_Surah_Al-Maeda_zmdlob.mp3",
+      "artist": "Idris Abkar",
+      "album": "Cloud Recitations",
+      "cover": "https://res.cloudinary.com/dnudhbjle/image/upload/q_auto/f_auto/v1781086631/idrees-abkar-banner_kmq9ly.jpg",
+      "isCloud": true
+    }, 
+    {
+      "id": "cloud-06",
+      "title": "Surah Al-Anaam",
+      "surahNo": 06,
+      "url": "https://res.cloudinary.com/dnudhbjle/video/upload/v1781099788/006_-_Surah_Al-Anaam_jy2cya.mp3",
+      "artist": "Idris Abkar",
+      "album": "Cloud Recitations",
+      "cover": "https://res.cloudinary.com/dnudhbjle/image/upload/q_auto/f_auto/v1781086631/idrees-abkar-banner_kmq9ly.jpg",
+      "isCloud": true
+    },
+    {
+      "id": "cloud-07",
+      "title": "Surah Al-Araf",
+      "surahNo": 07,
+      "url": "https://res.cloudinary.com/dnudhbjle/video/upload/v1781099791/007_-_Surah_Al-Araf_lrlxo8.mp3",
+      "artist": "Idris Abkar",
+      "album": "Cloud Recitations",
+      "cover": "https://res.cloudinary.com/dnudhbjle/image/upload/q_auto/f_auto/v1781086631/idrees-abkar-banner_kmq9ly.jpg",
+      "isCloud": true
+    },
+    {
+      "id": "cloud-08",
+      "title": "Surah Al-Anfal",
+      "surahNo": 08,
+      "url": "https://res.cloudinary.com/dnudhbjle/video/upload/v1781099769/008_-_Surah_Al-Anfal_fsqp7q.mp3",
+      "artist": "Idris Abkar",
+      "album": "Cloud Recitations",
+      "cover": "https://res.cloudinary.com/dnudhbjle/image/upload/q_auto/f_auto/v1781086631/idrees-abkar-banner_kmq9ly.jpg",
+      "isCloud": true
+    }, 
+    {
+      "id": "cloud-09",
+      "title": "Surah At_Tawbah",
+      "surahNo": 09,
+      "url": "https://res.cloudinary.com/dnudhbjle/video/upload/v1781099787/009_-_Surah_at_Tawbah_kfiaxi.mp3",
+      "artist": "Idris Abkar",
+      "album": "Cloud Recitations",
+      "cover": "https://res.cloudinary.com/dnudhbjle/image/upload/q_auto/f_auto/v1781086631/idrees-abkar-banner_kmq9ly.jpg",
+      "isCloud": true
+    },       
      {
       "id": "cloud-92",
       "title": "Surah Al-Lail",
